4ec75fe1af9bc5692d8d64af21d385dba15b580a
Msarmi84 <msarmi84@hotmail.com>
2021-06-08 08:33:19 +0200

    multer

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -62,3 +62,20 @@ const storage = multer.diskStorage({
 const upload = multer({ storage: storage }).single('img');
 
 module.exports = upload;
+
+// const multer = require('multer');
+// const path = require('path');
+
+// const storage = multer.diskStorage({
+//     destination: function(req, file, cb) {
+//         cb(null, 'public/images');
+//     },
+
+//     filename: function(req, file, cb) {
+//         cb(null, Date.now() + path.extname(file.originalname));
+//     }
+// });
+
+// const upload = multer({ storage: storage }).single('img');
+
+// module.exports = upload;
